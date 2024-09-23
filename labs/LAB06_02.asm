2a91012698fedbc9259b4ed561a29c427dd1165d
snow <bernardoatadia0609@gmail.com>
2024-09-23 13:27:42 -0300

    atv

--- labs/LAB06_02.asm
+++ labs/LAB06_02.asm
@@ -1,35 +1,33 @@
 title repeat
 .model Small 
-.code 
+.code
 
-main proc 
+main proc
 
-mov cx, 0 ; iniciar contador 
-mov ah,1 
+         mov  cx, 0      ; iniciar contador
+         mov  ah,1
 
-l1:
+    l1:  
 
-int 21h 
-inc cx ; incrementar contador 
-cmp al,0dh
-jne l1 
+         int  21h
+         inc  cx         ; incrementar contador
+         cmp  al,0dh     ; comparar CR
+         jne  l1
 
-fim: 
-mov ah,2   ; imprimir carac
+    fim: 
+         mov  ah,2       ; imprimir carac
 
-pl:
-mov  dl, '*'
-int  21h         ; loop imprimir '*'
-loop pl
+    pl:  
+         mov  dl, '*'
+         int  21h        ; loop imprimir '*'
+         loop pl
 
-main endp 
+main endp
 
-mov ah,4ch      ;finalizar programa 
-int 21h 
+         mov  ah,4ch     ;finalizar programa
+         int  21h
 
 end main 
-
-
 
 
 
