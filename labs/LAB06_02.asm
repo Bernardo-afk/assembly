d98e2333be4f3b80cd90c4dec91f6d53c30f8c5f
snow <bernardoatadia0609@gmail.com>
2024-09-23 10:08:06 -0300

    .

--- labs/LAB06_02.asm
+++ labs/LAB06_02.asm
@@ -3,34 +3,29 @@ title repeat
 .data
 .code 
 
-
 main proc 
 
 mov cx, -1 ; iniciar contador 
-
 mov ah,1 
+
 l1:
 
 int 21h 
 inc cx ; incrementar contador 
 cmp al,0dh
-
 jne l1 
 
-
 fim: 
-mov ah,2
+mov ah,2   ; imprimir carac
 
 pl:
-         mov  dl, '*'
-         int  21h         ; loop imprimir '*'
-         loop pl
-
-
+mov  dl, '*'
+int  21h         ; loop imprimir '*'
+loop pl
 
 main endp 
 
-mov ah,4ch 
+mov ah,4ch      ;finalizar programa 
 int 21h 
 
 end main 
