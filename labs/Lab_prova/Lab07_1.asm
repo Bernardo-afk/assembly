3a69091a53854b2c7335315b3e1c8ed897108214
snow <bernardoatadia0609@gmail.com>
2024-09-30 14:44:08 -0300

    lab7.1 feito

--- labs/Lab_prova/Lab07_1.asm
+++ labs/Lab_prova/Lab07_1.asm
@@ -2,73 +2,72 @@
 .stack 100h
 .data
 
-msg1 db 10,13, "Digite o dividendo: $"
-msg2 db 10,13, "Digite o divisor: $"
-msg3 db 10,13, "Quociente: $"
-msg4 db 10,13, "Resto: $"
+    msg1 db 10,13, "Digite o dividendo: $"
+    msg2 db 10,13, "Digite o divisor: $"
+    msg3 db 10,13, "Quociente: $"
+    msg4 db 10,13, "Resto: $"
 
 .code
 
 main proc
 
-    mov ax, @data
-    mov ds, ax
+         mov ax, @data
+         mov ds, ax
 
     ; Mostrar string dividendo
-    mov ah, 9
-    lea dx, msg1
-    int 21h
+         mov ah, 9
+         lea dx, msg1
+         int 21h
 
-    mov ah, 1
-    int 21h
-    sub al, 30h ; Converter em Número 
-     mov bl, al ; Salvar o dividendo em Bl
-    
+         mov ah, 1
+         int 21h
+         mov bl, al       ; Salvar o dividendo em Bl
+         sub bl, 30h      ; Converter em Número
    
 
     ; mostrar string divisor
-    mov ah, 9
-    lea dx, msg2
-    int 21h
+         mov ah, 9
+         lea dx, msg2
+         int 21h
 
-    mov ah, 1
-    int 21h
-      mov cl, al ; store in bl
-    sub cl, 30h ; convert to numeric value
+         mov ah, 1
+         int 21h
+         mov cl, al       ; guardar em cl
+         sub cl, 30h      ; converter em número
   
 
     ; Loop de subtração
-l1:
-    cmp bl, cl
-    jl fim
-    sub bl, cl
-    inc bh
-    jmp l1
+    l1:  
+         cmp bl, cl       ; comparar , para se satisfazer a confição jl
+         jl  fim
+         sub bl, cl       ; subtrair ( enunciado )
+         inc bh
+         jmp l1
 
-fim:
-    ; Output quotient
-    mov ah, 9
-    lea dx, msg3
-    int 21h
+    fim: 
+    ; Mostrar string do  quociente
+         mov ah, 9
+         lea dx, msg3
+         int 21h
 
-    add bh, 30h ; convert to ASCII
-    mov ah, 2
-    mov dl, bh
-    int 21h
+         add bh, 30h      ; converter para ASCII
+         mov ah, 2
+         mov dl, bh
+         int 21h
 
-    ; Output remainder
-    mov ah, 9
-    lea dx, msg4
-    int 21h
+    ; mostrar string  de resto
+         mov ah, 9
+         lea dx, msg4
+         int 21h
 
-    add bl, 30h ; convert to ASCII
-    mov ah, 2
-    mov dl, bl
-    int 21h
+         add bl, 30h      ; converter para ASCII
+         mov ah, 2
+         mov dl, bl
+         int 21h
 
-    ; Exit
-    mov ah, 4ch
-    int 21h
+    ; fim
+         mov ah, 4ch
+         int 21h
 
 main endp
 end main
