c924ad7fea899f81cf5cbbaf6ff9c8dea7ddc801
snow <bernardoatadia0609@gmail.com>
2024-10-18 17:08:31 -0300

    .

--- asm.asm
+++ asm.asm
@@ -1,39 +1,20 @@
-TITLE INVERTE_VETOR
-.model small
-.stack 100h
-.data
-    MSG1 db 10,13,"Insira os valores do vetor (1 por vez e de 0 a 9) $"
-    MSG2 DB 10,13,"O vetor inserido foi: $"
-    MSG3 db 10,13,"O vetor invertido eh: $"
-    vetor db 7 DUP(?)
-.code
-    main proc
-        
-    mov ax,@data
-    mov ds,ax ;Libera acesso ao data
+.model samll 
 
-    ;xor cx,cx ;Limpa cx
-    ;mov cx,7 ;Inicializa cx com o tamanho do vetor
 
-    xor si,si
+.data 
 
-    pegaVetor:
-        
-        mov ah,9
-        lea dx,MSG1 ;Imprime a mensagem 1
-        int 21h
+b db 10,13, "digite um número binario "
 
-        mov ah,1
-        int 21h ;Pega o caracter digitado em salva em AL
+.code 
 
-        mov vetor[si],al ;Passa o caracter lido para o vetor
-        inc si ;Passa para a próxima posição do vetor
+manin proc 
 
-        cmp si,6
-        jbe pegaVetor ;Roda até a ultima posição do vetor
+mova x,@data
+mov ds,ax
 
-    mov ah,4ch
-    int 21h
 
-    main endp
-    end main
+mov ah,9 
+lea dx,b
+int 21h 
+
+mov ah,
