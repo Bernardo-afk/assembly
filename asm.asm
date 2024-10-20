6383145f7276d5622d2549edfe82431b972a5828
snow <bernardoatadia0609@gmail.com>
2024-10-20 11:11:15 -0300

    .

--- asm.asm
+++ asm.asm
@@ -1,6 +1,25 @@
 .model small
 
-.data 
+.data
+    msg  db 10,13, "Ola $"
+
+.code
+
+main proc
 
 
-.code 
+         mov ax,@data
+         mov ds,ax
+
+
+         mov ah,9
+         lea dx,msg
+         int 21h
+         jmp Fim
+
+main endp
+    Fim: 
+         mov ah,4ch
+         int 21h
+
+end main 
